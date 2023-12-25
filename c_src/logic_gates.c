c0322213118714e54bb96a615ce8dfd8d34b9ee6
8kyu <guillemsenabre@gmail.com>
2023-12-26 00:13:48 +0800

    main.c --> logic_gates.c | added AND, NAND, OR, NOR

---
+++ c_src/logic_gates.c
@@ -0,0 +1,17 @@
+#include "common_module.h"
+
+int AND(int input1, int input2) {
+  return input1 && input2;
+}
+
+int NAND(int input1, int input2) {
+  return !(input1 && input2);
+}
+
+int OR(int input1, int input2) {
+  return input1 || input2;
+}
+
+int NOR(int input1, int input2) {
+  return !(input1 || input2);
+}
