c323166c68ac12514df6d7900655c60e71dc20a2
8kyu <guillemsenabre@gmail.com>
2023-12-26 00:37:11 +0800

    binary validation added to and_gate and logic validation corrected

--- c_src/logic_gates.c
+++ c_src/logic_gates.c
@@ -1,12 +1,18 @@
 #include "common_module.h"
 #include "stdbool.h"
+#include "stdio.h"
 
 // If the compiler doesn't support stdbool.h header
 #define True
 #define False 
 
 int and_gate(int input1, int input2) {
-  return input1 & input2;
+  if (valid_binary_input(input1, input2)) {
+    return input1 & input2;
+  } else {
+    printf("Not a binary number");
+    return -1;
+  }
 }
 
 int nand_gate(int input1, int input2) {
@@ -26,6 +32,6 @@ int xor_gate(int input1, int input2) {
 }
 
 bool valid_binary_input(int input1, int input2) {
-  return (input1 != 0 && input1 != 1 && input2 != 0 && input2 != 1);
+  return (input1 == 0 || input1 == 1 || input2 == 0 || input2 == 1);
 }
 
