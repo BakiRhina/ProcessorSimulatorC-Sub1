a91a45b95ce2cabf60b3701caf866fd44f8866ae
8kyu <guillemsenabre@gmail.com>
2023-12-26 00:17:21 +0800

    logic operand correction

--- c_src/logic_gates.c
+++ c_src/logic_gates.c
@@ -1,19 +1,19 @@
 #include "common_module.h"
 
 int AND(int input1, int input2) {
-  return input1 && input2;
+  return input1 & input2;
 }
 
 int NAND(int input1, int input2) {
-  return !(input1 && input2);
+  return !(input1 & input2);
 }
 
 int OR(int input1, int input2) {
-  return input1 || input2;
+  return input1 | input2;
 }
 
 int NOR(int input1, int input2) {
-  return !(input1 || input2);
+  return !(input1 | input2);
 }
 
 int XOR(int input1, int input2) {
