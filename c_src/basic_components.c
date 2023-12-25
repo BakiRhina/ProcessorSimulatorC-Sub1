5133970b162429b578549209f0399cb42577b3d6
8kyu <guillemsenabre@gmail.com>
2023-12-26 01:19:01 +0800

    added basic_components.c for multiplexers and others | updated common_module.h

---
+++ c_src/basic_components.c
@@ -0,0 +1,7 @@
+#include "common_module.h"
+#include "stdio.h"
+
+
+int multiplexer_2b(int input1, int input2, int select) {
+  
+}
---
+++ include/common_module.h
@@ -0,0 +1,22 @@
+#ifndef COMMON_MODULE_H
+#define COMMON_MODULE_H
+
+// Declarations for logic gates
+int and_gate(int input1, int input2);
+int nand_gate(int input1, int input2);
+int or_gate(int input1, int input2);
+int nor_gate(int input1, int input2);
+int xor_gate(int input1, int input2);
+
+// Declaration of basic components
+
+int multiplexer_2b(int input1, int input2, int select);
+
+
+
+
+
+
+
+
+#endif // COMMON_MODULE_H
