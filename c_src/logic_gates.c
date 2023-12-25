535cf5b3e5ddd62ec9cbf63bb8aa4d5014eb867b
8kyu <guillemsenabre@gmail.com>
2023-12-26 00:29:35 +0800

    added binary validation

--- c_src/logic_gates.c
+++ c_src/logic_gates.c
@@ -1,4 +1,8 @@
 #include "common_module.h"
+#include "stdbool.h"
+
+#define True
+#define False
 
 int AND(int input1, int input2) {
   return input1 & input2;
@@ -17,6 +21,10 @@ int NOR(int input1, int input2) {
 }
 
 int XOR(int input1, int input2) {
-  return !(input1 ^ input2);
+  return input1 ^ input2;
+}
+
+bool valid_binary_input(int input1, int input2) {
+  return (input1 != 0 && input1 != 1 && input2 != 0 && input2 != 1);
 }
 
