e67a8d37538788765e8d7b53d5aac6deaa74d6e6
8kyu <guillemsenabre@gmail.com>
2023-12-26 14:02:16 +0800

    added 3 bits and gate

--- c_src/logic_gates.c
+++ c_src/logic_gates.c
@@ -7,6 +7,8 @@
 #define False 
 
 
+//SECTION - AND gates
+// 2 bits AND gate
 int and_gate_2bit(int input1, int input2) {
   if (valid_binary_input(input1, input2)) {
     return input1 & input2;
@@ -16,6 +18,17 @@ int and_gate_2bit(int input1, int input2) {
   }
 }
 
+// 3 bits AND gate
+int and_gate_4bit(int input1, int input2, int input3) {
+  if (valid_binary_input(input1, input2)) {
+    return input1 & input2 & input3;
+  } else {
+    printf("Not a binary number");
+    return -1;
+  }
+}
+
+// 4 bits AND gate
 int and_gate_4bit(int input1, int input2, int input3, int input4) {
   if (valid_binary_input(input1, input2)) {
     return input1 & input2 & input3 & input4;
@@ -24,6 +37,8 @@ int and_gate_4bit(int input1, int input2, int input3, int input4) {
     return -1;
   }
 }
+
+//!SECTION
 
 int nand_gate(int input1, int input2) {
   if (valid_binary_input(input1, input1)) {
