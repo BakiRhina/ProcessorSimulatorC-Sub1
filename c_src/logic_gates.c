73c85f209094775a40b299aa73051aa67377fdd6
8kyu <guillemsenabre@gmail.com>
2023-12-26 14:50:20 +0800

    added variable nand gate

--- c_src/logic_gates.c
+++ c_src/logic_gates.c
@@ -1,10 +1,6 @@
 #include "common_module.h"
-#include "stdbool.h"
+#include "c_module1.h"
 #include "stdio.h"
-
-// If the compiler doesn't support stdbool.h header
-#define True
-#define False 
 
 
 //SECTION - AND gates
@@ -36,30 +32,51 @@ int and_gate_4bit(int input1, int input2, int input3, int input4) {
 
 // Variable input number AND gate
 
-  int and_gate_var(int num_inputs, int inputs[]) {
-    int result = inputs[0]; // initialize result with first value of the input 0 or 1.
-    
-    for (int i = 1; i < num_inputs; ++i) {
-      if (!valid_binary_input(inputs[i])) {
-        printf("Input at index %d is not a binary\n", i);
-        return -1;
-      } else {
-        result &= inputs[i]; // If result is not initialized (empty) the operator & will lead to errors.
-      }
+int and_gate_var(int num_inputs, int inputs[]) {
+  int result = inputs[0]; // initialize result with first value of the input 0 or 1.
+  
+  for (int i = 1; i < num_inputs; ++i) {
+    if (!valid_binary_input(inputs[i])) {
+      printf("Input at index %d is not a binary\n", i);
+      return -1;
+    } else {
+      result &= inputs[i]; // If result is not initialized (empty) the operator & will lead to errors.
     }
-    return result;
   }
+  return result;
+}
 
 //!SECTION
 
+//SECTION - NAND gates
+
+
+// 2 bit NAND gate
 int nand_gate(int input1, int input2) {
-  if (valid_binary_input(input1, input1)) {
+  if (valid_binary_input(input1, input2)) {
     return !(input1 & input2);
   } else {
     printf("Not a binary number");
     return -1;
   }
 }
+
+// Variable input number NAND gate
+int nand_gate_var(int num_inputs, int inputs[]) {
+  int result = inputs[0];
+  
+  for (int i = 1; i < num_inputs; ++i) {
+    if (!valid_binary_input(inputs[i])) {
+      printf("Input at index %d is not a binary\n", i);
+      return -1;
+    } else {
+      result &= inputs[i];
+    }
+  }
+  return !result;
+}
+
+//!SECTION
 
 int or_gate(int input1, int input2) {
   if (valid_binary_input(input1, input2)) {
