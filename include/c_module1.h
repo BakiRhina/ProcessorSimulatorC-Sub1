a8aa98c65a1acaeb511b5f858a3a1816ef6b9911
8kyu <guillemsenabre@gmail.com>
2023-12-26 12:34:50 +0800

    moved instances of gate functions to c_module1.h

---
+++ include/c_module1.h
@@ -0,0 +1,12 @@
+#ifndef C_MODULE1_H
+#define C_MODULE1_H
+
+// Declarations for logic gates
+int and_gate(int input1, int input2);
+int nand_gate(int input1, int input2);
+int or_gate(int input1, int input2);
+int nor_gate(int input1, int input2);
+int xor_gate(int input1, int input2);
+int not_gate(int input);
+
+#endif
--- include/common_module.h
+++ include/common_module.h
@@ -6,13 +6,6 @@
 #ifndef COMMON_MODULE_H
 #define COMMON_MODULE_H
 
-// Declarations for logic gates
-int and_gate(int input1, int input2);
-int nand_gate(int input1, int input2);
-int or_gate(int input1, int input2);
-int nor_gate(int input1, int input2);
-int xor_gate(int input1, int input2);
-int not_gate(int input);
 
 // Declaration of basic components
 int multiplexer_2b(int input1, int input2, int select);
