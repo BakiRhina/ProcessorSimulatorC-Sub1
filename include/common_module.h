14d5e867d1b1bd9392944de741d908a956ac41f1
8kyu <guillemsenabre@gmail.com>
2023-12-26 15:12:26 +0800

    instance in common header and comment

--- include/common_module.h
+++ include/common_module.h
@@ -10,7 +10,7 @@
 // Declaration of basic components
 int multiplexer_2b(int input1, int input2, int select);
 int multiplexer_2b_nand(int input1, int input2, int select);
-
+int multiplexer_4b(int input1, int input2, int input3, int input4, int s0, int s1);  // Soon to be changed into arrays inputs[4], select [2]
 
 
 #endif // COMMON_MODULE_H
